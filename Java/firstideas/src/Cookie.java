eb30ca9a06e25472210d226c43a0cf82c1a91d22
Aesir <eduardo.arcos@epn.edu.ec>
2023-05-21 16:38:31 -0500

    First steps on object oriented programming

---
+++ Java/firstideas/src/Cookie.java
@@ -0,0 +1,15 @@
+public class Cookie {
+    String flavor;
+    boolean glaze;
+    Companion dessert = new Companion();
+
+    boolean getGlaze() {
+        return glaze;
+    }
+    Companion getDessert() {
+        return dessert;
+    }
+
+
+}
+
