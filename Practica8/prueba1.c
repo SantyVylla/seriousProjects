5fed041a64f0651c6e671e67e3520d60a6650d33
l0nging <118124980+l0nging@users.noreply.github.com>
2023-07-14 14:03:36 +0000

    practica

---
+++ Practica8/prueba1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    while (1)
+    {
+        int *ptr=malloc(1024);
+        if (ptr == NULL)
+        {
+            printf("No se puede asignar memoria\n");
+            return EXIT_FAILURE;
+        }
+        *ptr = 5;
+        printf("El valor es %d\n", *ptr);
+    }
+}
